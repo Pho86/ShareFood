32dd335e46738cfd8a56e2adc665031537b4eae4
William <wchu41@my.bcit.ca>
2022-04-06 23:02:11 -0700

    imgs and skip text

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,12 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import Text from '../comps/Text.js'
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+    <Text>Skip</Text>
     </div>
   )
 }
