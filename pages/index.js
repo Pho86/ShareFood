9b7b37b79ceff002e91bfd867703cd70dbda80cb
Philip Ho <pho86@se14l0124-08.ad.bcit.ca>
2022-04-07 16:17:18 -0700

    next button/progress bar

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {useRouter} from 'next/router'
 
+import Button from '../comps/Button'
+import ProgressBar from '../comps/Progress'
 import Text from '../comps/Text.js'
 
 export default function Home() {
   return (
     <div className={styles.container}>
     <Text>Skip</Text>
+    <Button></Button>
+    <ProgressBar></ProgressBar>
     </div>
   )
 }
