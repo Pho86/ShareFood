750faa63e83a2834315431f8d0a54542b7cc2bf7
William <wchu41@my.bcit.ca>
2022-04-15 00:38:05 -0700

    text in confirm page

---
+++ data/confirm_content.js
@@ -0,0 +1,23 @@
+export const categories = [
+    {
+        title: "Name of Food",
+        fill: "Peanut Butter"
+    },
+    {
+        title: "Weight",
+        fill: "0.5 lbs"
+    },
+    {
+        title: "Date of Purchase",
+        fill: "Mar 26, 2022"
+    },
+    {
+        title: "Best Before Date",
+        fill: "Apr 2, 2022"
+    },
+    {
+        title: "Additional Details",
+        fill: "\"Brand New\""
+    },
+]
+// idk if i need this
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,14 +1,15 @@
 import styles from '../styles/Home.module.css'
 import { useRouter } from 'next/router'
+import Text from '../comps/Text.js'
+import ConfirmText from '../comps/ConfirmText';
+
 
 export default function Home() {
     const r = useRouter();
     const { route } = r.query
   
     return (
-      <div className={styles.container}>
-      confirm page here
-      </div>
+      <ConfirmText/>
     )
   }
   
