4de278409ba725f9fa51d9217ad9afd22f1c333d
Philip Ho <pho86@se14l0124-08.ad.bcit.ca>
2022-04-21 14:21:39 -0700

    chat/progress bar

--- comps/Progress.js
+++ comps/Progress.js
@@ -14,12 +14,79 @@ background-color:#F1F1F1;
 border-radius:50%;
 `
 
+const ProgressFill = styled.div`
+width:25px;
+height:25px;
+background-color:#D2D2D2;
+border-radius:50%
+`
+
 export default function ProgressBar() {
-    return <ProgressCont>
-        <Progress></Progress>
+    const r = useRouter();
+    var { tut } = r.query;
+    if (tut === undefined) {
+        tut = 0;
+    }
+    if (tut === '0') {
+        return <ProgressCont>
+        <ProgressFill></ProgressFill>
         <Progress></Progress>
         <Progress></Progress>
         <Progress></Progress>
         <Progress></Progress>
     </ProgressCont>
+
+    }
+    if (tut === '1') {
+        return <ProgressCont>
+            <Progress></Progress>
+            <ProgressFill></ProgressFill>
+            <Progress></Progress>
+            <Progress></Progress>
+            <Progress></Progress>
+        </ProgressCont>
+
+    }
+    if (tut === '2') {
+        return <ProgressCont>
+            <Progress></Progress>
+            <Progress></Progress>
+            <ProgressFill></ProgressFill>
+            <Progress></Progress>
+            <Progress></Progress>
+        </ProgressCont>
+
+    }
+    if (tut === '3') {
+        return <ProgressCont>
+            <Progress></Progress>
+            <Progress></Progress>
+            <Progress></Progress>
+            <ProgressFill></ProgressFill>
+            <Progress></Progress>
+        </ProgressCont>
+
+    }
+    if (tut === '4') {
+        return <ProgressCont>
+            <Progress></Progress>
+            <Progress></Progress>
+            <Progress></Progress>
+            <Progress></Progress>
+            <ProgressFill></ProgressFill>
+        </ProgressCont>
+
+    }
+    else {
+        return <ProgressCont>
+        <ProgressFill></ProgressFill>
+        <Progress></Progress>
+        <Progress></Progress>
+        <Progress></Progress>
+        <Progress></Progress>
+    </ProgressCont>
+    }
+
+
+
 }
