cdb83fba8f70278b8e3915a1c1aeef2ae4c74c0d
William <wchu41@my.bcit.ca>
2022-04-25 23:02:41 -0700

    fixed soft code

--- data/confirm_content.js
+++ data/confirm_content.js
@@ -9,7 +9,7 @@ export const categories1 = [
         title: "Date of Purchase"
     },
     {
-        title: "Best Before Date"
+        title: "Best Before text"
     },
     {
         title: "Pick-up Location"
@@ -19,23 +19,130 @@ export const categories1 = [
     },
 ]
 
-export const categories2 = [
+export const nameoffood = [
     {
-        fill: "Soy Sauce"
+        text: "Peanut Butter"
     },
     {
-        fill: "0.6 lbs"
+        text: "Box of Cereal"
     },
     {
-        fill: "Mar 26, 2022"
+        text: "Egg Carton"
     },
     {
-        fill: "Apr 2, 2022"
+        text: "Instant Noodles"
     },
     {
-        fill: "1242 Main St."
+        text: "Soy Sauce"
+    }
+]
+
+
+export const weight = [
+    {
+        text: "0.5 lbs"
     },
     {
-        fill: "\"Brand New\""
+        text: "0.3 lbs"
     },
+    {
+        text: "1.5 lbs"
+    },
+    {
+        text: "0.1 lbs"
+    },
+    {
+        text: "0.6 lbs"
+    }
+]
+
+export const dop = [
+    {
+        text: "Mar 26, 2022"
+    },
+    {
+        text: "Mar 11, 2022"
+    },
+    {
+        text: "Feb 26, 2022"
+    },
+    {
+        text: "Apr 6, 2022"
+    },
+    {
+        text: "Apr 13, 2022"
+    }
+]
+
+export const bbd = [
+    {
+        text: "Apr 2, 2022"
+    },
+    {
+        text: "Apr 11, 2022"
+    },
+    {
+        text: "Mar 17, 2022"
+    },
+    {
+        text: "Apr 26, 2022"
+    },
+    {
+        text: "May 23, 2022"
+    }
+]
+
+export const location = [
+    {
+        text: "1251 Main St."
+    },
+    {
+        text: "983 White Dr."
+    },
+    {
+        text: "402 Cambie St."
+    },
+    {
+        text: "15 Yemen Road"
+    },
+    {
+        text: "4127 Burnaby Dr."
+    }
+]
+
+export const details = [
+    {
+        text: "\"Opened, but never used\""
+    },
+    {
+        text: "\"Brand new, unopened.\""
+    },
+    {
+        text: "\"Unopened.\""
+    },
+    {
+        text: "\"I have extras if you want more\""
+    },
+    {
+        text: "\"On a diet.\""
+    }
+]
+
+//names of the person giving away food
+export const names = [
+    {
+        text: "Amy Mo"
+    },
+    {
+        text: "Chad Bruno"
+    },
+    {
+        text: "Li Lee"
+    },
+    {
+        text: "Hope Chen"
+    },
+    {
+        text: "Jessica"
+    }
 ]
