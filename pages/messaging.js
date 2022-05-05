4caeedc8d5b8c1feb2fca17a9b41eeb49c25d442
Philip <pho86@my.bcit.ca>
2022-05-04 17:08:53 -0700

    details/weight fix

--- pages/messaging.js
+++ pages/messaging.js
@@ -4,20 +4,22 @@ import NavBar, { ChatNavbar } from '../comps/NavBar';
 import Text, { Message, SendIcon, Space } from '../comps/Text';
 import { chat } from '../data/chat_content';
 import { PersonChat, PersonBubble, MeBubble, MeChat } from '../comps/Chat';
+import foody from '../data/food_content.json';
+import TutorialText from '../comps/TutorialText'
 
 export default function Home() {
     const r = useRouter();
-    const { route } = r.query
+    var { food } = r.query
 
     return (
         <div className={styles.container}>
-
-            <div className={styles.chat}>
-                <div class="message"><PersonBubble></PersonBubble><PersonChat text={chat[0].chat} />
+            <TutorialText text={foody[food].name} />
+            <div class="chat">
+                <div class="message"><PersonBubble name={foody[food].initals}></PersonBubble><PersonChat text= {"Hi thanks for deciding to come get my " + foody[food].food} />
                 </div>
                 <div class="message1"><MeChat text={chat[1].chat} /><MeBubble name={chat[1].initals}  ></MeBubble>
                 </div>
-                <div class="message"><PersonBubble ></PersonBubble><PersonChat text={chat[2].chat} />
+                <div class="message"><PersonBubble name={foody[food].initals}></PersonBubble><PersonChat text={chat[2].chat} />
                 </div>
             </div>
             {/*  <Space is temporarily used to move msg box down */}
@@ -31,3 +33,5 @@ export default function Home() {
         </div>
     )
 }
+
+
